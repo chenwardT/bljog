dd751ba156cc139e869a4caf47fdac2de9c5dda8
Edward Chen <chenward.t@gmail.com>
2015-04-22 18:03:33 -0400

    Model migrations, set TZ.

--- bljog/blog/models.py
+++ bljog/blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.contenttypes import generic
+
+User = settings.AUTH_USER_MODEL
 
 
 class Post(models.Model):
--- bljog/comments/models.py
+++ bljog/comments/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
+
+User = settings.AUTH_USER_MODEL
 
 
 class Comment(models.Model):
