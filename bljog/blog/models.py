f2b7a15144e9a49ba22e40fc9fa03057780a0c75
Edward Chen <chenward.t@gmail.com>
2015-04-22 17:29:56 -0400

    Post and Comment models, using ContentTypes.

--- bljog/blog/models.py
+++ bljog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.contenttypes import generic
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=80)
+    slug = models.SlugField(unique=True)
+    body = models.TextField(blank=True)
+    comments = generic.GenericRelation('Comment')
--- bljog/comments/models.py
+++ bljog/comments/models.py
@@ -2,3 +2,10 @@ from django.db import models
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
 
+
+class Comment(models.Model):
+    author = models.ForeignKey(User)
+    body = models.TextField(blank=True)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey()
