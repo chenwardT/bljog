8c307d01564bbe6713dc53b89fcd77287987ed92
Edward Chen <chenward.t@gmail.com>
2015-04-26 06:01:26 -0400

    Fixed deprecated import.

--- bljog/comments/models.py
+++ bljog/comments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 User = settings.AUTH_USER_MODEL
@@ -11,7 +11,7 @@ class Comment(models.Model):
     body = models.TextField(blank=True)
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey()
+    content_object = GenericForeignKey()
 
     def __str__(self):
         return self.body
