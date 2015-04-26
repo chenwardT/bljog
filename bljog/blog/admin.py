fdf0fc00cb860746cea62e3b0580f3e2094a6370
Edward Chen <chenward.t@gmail.com>
2015-04-26 08:12:21 -0400

    Blog admin site touch up.

--- bljog/blog/admin.py
+++ bljog/blog/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
+    fields = (('author', 'title', 'published'), 'body')
+    list_display = ('author', 'title', 'created', 'modified', 'published')
