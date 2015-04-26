6754fd06100bf2048173db08bf89bfaa860918b5
Edward Chen <chenward.t@gmail.com>
2015-04-26 07:23:09 -0400

    Trying out a Bootstrap blog template.

--- bljog/core/views.py
+++ bljog/core/views.py
@@ -4,5 +4,5 @@ from blog.models import Post
 
 
 def index(request):
-    recent_posts = Post.objects.all().order_by('-created')[:5]
+    recent_posts = Post.objects.all().order_by('-created')[:3]
     return render(request, 'core/home.html', context={'recent_posts': recent_posts})
