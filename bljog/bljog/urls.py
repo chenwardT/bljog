e6d2cfa7c3e1a3ffac24a562caea39f8e103f3ae
Edward Chen <chenward.t@gmail.com>
2015-04-27 06:57:33 -0400

    Started class-based view implementation.

--- bljog/bljog/urls.py
+++ bljog/bljog/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from core.views import HomeView
+
+# TODO: Name views and factor out into per-app includes.
 urlpatterns = [
     # Examples:
     # url(r'^$', 'bljog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'core.views.index', name='index'),
+    url(r'^$', HomeView.as_view()),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
--- bljog/core/views.py
+++ bljog/core/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
+from django.views.generic.base import ContextMixin, TemplateView
 
 from blog.models import Post
+from .models import Link
 
 
-def index(request):
-    recent_posts = Post.objects.all().order_by('-created')[:3]
-    return render(request, 'core/home.html', context={'recent_posts': recent_posts})
+# def index(request):
+#     recent_posts = Post.objects.all().order_by('-created')[:3]
+#     return render(request, 'core/home.html', context={'recent_posts': recent_posts})
+
+
+class ArchivedPostsMixin(ContextMixin):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['archived_posts'] = Post.objects.list_archived_months()
+
+        return context
+
+
+class LinksMixin(ContextMixin):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['links'] = Link.objects.all()
+
+        return context
+
+
+class RecentPostsMixin(ContextMixin):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recent_posts'] = Post.objects.order_by('-created')[:5]
+
+        return context
+
+
+class HomeView(RecentPostsMixin, LinksMixin, ArchivedPostsMixin, TemplateView):
+    template_name = 'core/home.html'
